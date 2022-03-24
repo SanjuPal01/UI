fdb6ded527b27c004375cee56b98e270e5852611
SanjuPal01 <sanju.sanju@progress.com>
2022-03-24 21:02:18 +0530

    Angular

---
+++ Angular/scripts/10-clickableHeader.js
@@ -0,0 +1,43 @@
+/// <reference path="angular.js"/>
+
+angular
+.module("myModule", [])
+.controller("myController", function($scope){
+    var employees = [
+        {
+            name: "Ben", dateOfBirth: new Date("November 23, 1980"),
+            gender: "Male", salary: 55000
+        },
+        {
+            name: "Sara", dateOfBirth: new Date("May 05, 1970"),
+            gender: "Female", salary: 68000
+        },
+        {
+            name: "Mark", dateOfBirth: new Date("August 15, 1974"),
+            gender: "Male", salary: 57000
+        },
+        {
+            name: "Pam", dateOfBirth: new Date("October 27, 1979"),
+            gender: "Female", salary: 53000
+        },
+        {
+            name: "Todd", dateOfBirth: new Date("December 30, 1983"),
+            gender: "Male", salary: 60000
+        }
+    ];
+    $scope.employees = employees;
+    $scope.sortCol = 'name';
+    $scope.reverseSort = false;
+
+    $scope.sortData = function(column){
+        $scope.reverseSort = (column == $scope.sortCol) ? !$scope.reverseSort : false;
+        $scope.sortCol = column;
+    }
+
+    $scope.getSortClass = function(column){
+        if (column == $scope.sortCol){
+            return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+        }
+        return ''
+    }
+})
