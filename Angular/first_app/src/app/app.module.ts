d363814aee2552a728be79801ce2017d48b36ea2
SanjuPal01 <sanju.sanju@progress.com>
2022-04-12 15:26:55 +0530

    update

--- Angular/first_app/src/app/app.module.ts
+++ Angular/first_app/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { StructuralDirectiveComponent } from './structural-directive/structural-directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
+    StructuralDirectiveComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
